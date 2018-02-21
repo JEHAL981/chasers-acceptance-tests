f4668e09c1cf3b4c4eca5ca3e598fffd30c2049f
James Halls <JEHalls81@gmail.com>
2018-02-21 16:51:52 +0000

    Updated Page

--- src/test/claphamchasers/pages/login/Page.js
+++ src/test/claphamchasers/pages/login/Page.js
@@ -1,13 +1,6 @@
 'use strict';
 
 import conf from '../../../../../conf/env/wdio.conf';
-import {loginPage} from "../../locator/chasers/loginpage_locator";
-
-const  {
-    chasers_login_page_email_address_text_field
-
-} = loginPage;
-
 
 /**
  * Page file
